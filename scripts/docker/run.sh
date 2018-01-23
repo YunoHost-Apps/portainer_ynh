d4348e9ffae098d9126b43553c1c83a03a224e96
Développeur libre <aymhce@gmail.com>
2018-01-23 23:23:20 +0100

    remove special yunohost link in container

--- scripts/docker/run.sh
+++ scripts/docker/run.sh
@@ -7,7 +7,6 @@ image=portainer/portainer:1.14.2
 options="-p YNH_PORT:9000 -v YNH_DATA/data:/data"
 
 other_options="--add-host yunohost:YNH_HOST"
-[ "$incontainer" == "0" ] && other_options="--link YNH_ID:yunohost"
 
 docker run -d --name=YNH_APP --restart always $options $other_options $image >/dev/null 2>&1
 CR=$?
