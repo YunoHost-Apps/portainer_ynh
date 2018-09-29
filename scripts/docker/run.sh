ecb1d46e6d4acd7900bed068eb9ac736480b4703
Guillaume DELVIT <guiguid@free.fr>
2018-09-29 18:19:05 +0200

    Update run.sh
    fix

--- scripts/docker/run.sh
+++ scripts/docker/run.sh
@@ -5,11 +5,13 @@
 [ "$architecture" == "armhf" ] && image=portainer/portainer:linux-arm-1.19.1
 [ -z $image ] && ynh_die "Sorry, your $architecture architecture is not supported ..."
 
-options="-p $port:9000 -v $data_path/data:/data -v /var/run/docker.sock:/var/run/docker.sock --no-auth"
+options="-p $port:9000 -v $data_path/data:/data -v /var/run/docker.sock:/var/run/docker.sock"
+
+containeroptions="--no-auth"
 
 iptables -t filter -N DOCKER 
 
-docker run -d --name=$app --restart always $options $image 1>&2
+docker run -d --name=$app --restart always $options $image $containeroptions 1>&2
 CR=$?
 
 echo $CR
