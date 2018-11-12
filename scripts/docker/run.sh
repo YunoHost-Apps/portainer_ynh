be44adf760ad84f2bad06e1f6380f0025f58cbdb
Développeur libre <aymhce@gmail.com>
2018-11-12 23:11:36 +0100

    Update run.sh
    go on 1.19.2

--- scripts/docker/run.sh
+++ scripts/docker/run.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-[ "$architecture" == "amd64" ] && image=portainer/portainer:1.19.1
-[ "$architecture" == "i386" ]  && image=portainer/portainer:linux-386-1.19.1
-[ "$architecture" == "armhf" ] && image=portainer/portainer:linux-arm-1.19.1
+[ "$architecture" == "amd64" ] && image=portainer/portainer:1.19.2
+[ "$architecture" == "i386" ]  && image=portainer/portainer:linux-386-1.19.2
+[ "$architecture" == "armhf" ] && image=portainer/portainer:linux-arm-1.19.2
 [ -z $image ] && ynh_die "Sorry, your $architecture architecture is not supported ..."
 
 options="-p $port:9000 -v $data_path/data:/data -v /var/run/docker.sock:/var/run/docker.sock"
-
 containeroptions="--no-auth"
 
 iptables -t filter -N DOCKER 
