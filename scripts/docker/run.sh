d98487a3593a3051851c4a07eb3f6a5daf298072
dr41nU <50152843+dr41nU@users.noreply.github.com>
2019-05-30 21:53:43 +0200

    Not specifying arch

--- scripts/docker/run.sh
+++ scripts/docker/run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-[ "$architecture" == "amd64" ] && image=portainer/portainer:1.20.1
-[ "$architecture" == "i386" ]  && image=portainer/portainer:linux-386-1.20.1
-[ "$architecture" == "armhf" ] && image=portainer/portainer:linux-arm-1.20.1
-[ -z $image ] && ynh_die "Sorry, your $architecture architecture is not supported ..."
+#[ "$architecture" == "amd64" ] && image=portainer/portainer:1.20.1
+#[ "$architecture" == "i386" ]  && image=portainer/portainer:linux-386-1.20.1
+#[ "$architecture" == "armhf" ] && image=portainer/portainer:linux-arm-1.20.1
+#[ -z $image ] && ynh_die "Sorry, your $architecture architecture is not supported ..."
+
+image=portainer/portainer
 
 options="-p $port:9000 -v $data_path/data:/data -v /var/run/docker.sock:/var/run/docker.sock"
 containeroptions="--no-auth"
