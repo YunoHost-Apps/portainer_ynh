97ca1833407e41add0d740f0ed835170c0e321f8
Doc-Saintly <43477526+Doc-Saintly@users.noreply.github.com>
2020-01-20 22:23:19 +0800

    Update run.sh

--- scripts/docker/run.sh
+++ scripts/docker/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-[ "$architecture" == "amd64" ] && image=portainer/portainer:1.20.1
-[ "$architecture" == "i386" ]  && image=portainer/portainer:linux-386-1.20.1
-[ "$architecture" == "armhf" ] && image=portainer/portainer:linux-arm-1.20.1
+[ "$architecture" == "amd64" ] && image=portainer/portainer:1.23.0
+[ "$architecture" == "i386" ]  && image=portainer/portainer:linux-386-1.23.0
+[ "$architecture" == "armhf" ] && image=portainer/portainer:linux-arm-1.23.0
 [ -z $image ] && ynh_die "Sorry, your $architecture architecture is not supported ..."
 
 options="-p $port:9000 -v $data_path/data:/data -v /var/run/docker.sock:/var/run/docker.sock"
